3a2e7f2ed0b55a5ca70c451bc34d5f16e9ebbc5a
Pere Vallduriola <valldu@hotmail.com>
2025-02-13 09:27:51 +0100

    fix_: Se corrige la cantidad total a ingresar en el report

--- app/src/main/java/net/ifeu/edicards/Reporting_Totales.java
+++ app/src/main/java/net/ifeu/edicards/Reporting_Totales.java
@@ -189,7 +189,7 @@ public class Reporting_Totales extends Activity {
 
 		
     	// 	Cantidad Pendiente a ingresar
-    	double pendiente = cantidad - ingresos;
+    	double pendiente = cantidad - gastos;
     	layout2.addView(this.addCounter("Total PENDIENTE INGRESAR", df.format(pendiente) + " €", Color.BLACK, true));
     	
     	layout.addView(layout2);
