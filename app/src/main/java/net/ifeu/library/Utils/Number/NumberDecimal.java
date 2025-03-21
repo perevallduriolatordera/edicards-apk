236713bad5c7780e72f8e4106041fcc6ee81bb5d
Pere Vallduriola <valldu@hotmail.com>
2025-03-21 10:47:35 +0100

    fix: fix result with roundNearestFive

--- app/src/main/java/net/ifeu/library/Utils/Number/NumberDecimal.java
+++ app/src/main/java/net/ifeu/library/Utils/Number/NumberDecimal.java
@@ -4,6 +4,7 @@ public class NumberDecimal {
 
     public static float roundToNearestFive(double value) {
         // Dividimos el valor entre 5, redondeamos al entero más cercano y lo multiplicamos por 5
+        if (value < 2.5) return 0;
         if (value < 5) return 5;
         return Math.round(value / 5) * 5;
     }
